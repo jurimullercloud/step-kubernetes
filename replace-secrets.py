8f26f9806261a0c539b49b4eb0c8f80ac33c3bf7
mirzakhan.aliyev <mirzakhan.aliyev@technipenergies.com>
2022-03-03 21:14:08 +0400

    Improved Jenkinsfile for multiple runs

--- replace-secrets.py
+++ replace-secrets.py
@@ -1,4 +1,5 @@
 import sys
+import base64
 import yaml
 
 if __name__ == "__main__":
@@ -8,11 +9,11 @@ if __name__ == "__main__":
     with open("./phonebookapp-secrets.yaml", "r") as f:
         secrets_yaml = yaml.safe_load(f)
 
-    secrets_yaml["data"]["POSTGRES_USER"] = data["POSTGRES_USER"]
-    secrets_yaml["data"]["POSTGRES_PASSWORD"] = data["POSTGRES_PASSWORD"]
-    secrets_yaml["data"]["POSTGRES_DB"] = data["POSTGRES_DB"]
-    secrets_yaml["data"]["DB_SERVICE_NAME"] = data["DB_SERVICE_NAME"]
-    secrets_yaml["data"]["JWT_SECRET_KEY"] = data["JWT_SECRET_KEY"]
+    secrets_yaml["data"]["POSTGRES_USER"] = base64.b64encode(data["POSTGRES_USER"].encode("ascii")).decode("ascii")
+    secrets_yaml["data"]["POSTGRES_PASSWORD"] = base64.b64encode(data["POSTGRES_PASSWORD"].encode("ascii")).decode("ascii")
+    secrets_yaml["data"]["POSTGRES_DB"] = base64.b64encode(data["POSTGRES_DB"].encode("ascii")).decode("ascii")
+    secrets_yaml["data"]["DB_SERVICE_NAME"] = base64.b64encode(data["DB_SERVICE_NAME"].encode("ascii")).decode("ascii")
+    secrets_yaml["data"]["JWT_SECRET_KEY"] = base64.b64encode(data["JWT_SECRET_KEY"].encode("ascii")).decode("ascii")
 
     with open("./phonebookapp-secrets.yaml", "w") as f:
         yaml.dump(secrets_yaml, f)
